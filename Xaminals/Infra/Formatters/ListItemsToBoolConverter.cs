d81398b53c0584134d459028c930368bd74d624c
bangurja <banshi.gurjar@digitas.com>
2020-04-25 12:18:28 +0530

    dADAD

--- Xaminals/Infra/Formatters/ListItemsToBoolConverter.cs
+++ Xaminals/Infra/Formatters/ListItemsToBoolConverter.cs
@@ -10,7 +10,15 @@ namespace exchaup.Infra.Formatters
         public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
         {
             if (value is IList valueAsList)
+            {
+                if (parameter!=null && bool.TryParse(parameter.ToString(), out bool zeroLengthExpect))
+                {
+                    if (!zeroLengthExpect)
+                        return valueAsList.Count > 0;
+                }
+
                 return valueAsList.Count == 0;
+            }
 
             return false;
         }
