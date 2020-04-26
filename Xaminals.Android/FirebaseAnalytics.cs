5e3c3399655db60f4412cf4b1ee7f6c5756b5ad5
bangurja <banshi.gurjar@digitas.com>
2020-04-26 14:47:15 +0530

    Anakytcs

---
+++ Xaminals.Android/FirebaseAnalytics.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using Android.OS;
+using exchaup;
+using Firebase.Analytics;
+using Xamarin.Forms;
+using Xaminals.Droid;
+
+[assembly: Xamarin.Forms.Dependency(typeof(FirebaseAnalyticsCommon))]
+namespace Xaminals.Droid
+{
+
+    public class FirebaseAnalyticsCommon : IFirebaseAnalytics
+    {
+        public void SendEvent(string eventId)
+        {
+            SendEvent(eventId, null);
+        }
+
+        public void SendEvent(string eventId, string paramName, string value)
+        {
+            SendEvent(eventId, new Dictionary<string, string>
+            {
+                {paramName, value}
+            }); ;
+        }
+
+        public void SendEvent(string eventId, IDictionary<string, string> parameters)
+        {
+            var firebaseAnalytics = FireaseAnalytics.GetInstance(Forms.Context);
+
+            if (parameters == null)
+            {
+                firebaseAnalytics.LogEvent(eventId, null);
+                return;
+            }
+
+            var bundle = new Bundle();
+            foreach (var param in parameters)
+            {
+                bundle.PutString(param.Key, param.Value);
+            }
+
+            firebaseAnalytics.LogEvent(eventId, bundle);
+        }
+    }
+}
---
+++ Xaminals/IFirebaseAnalytics.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+
+namespace exchaup
+{
+    public interface IFirebaseAnalytics
+    {
+        void SendEvent(string eventId);
+        void SendEvent(string eventId, string paramName, string value);
+        void SendEvent(string eventId, IDictionary<string, string> parameters);
+    }
+}
