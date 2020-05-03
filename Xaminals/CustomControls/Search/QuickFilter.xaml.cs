c03b67dad60f4e25defffa485782d409ea87f607
bangurja <banshi.gurjar@digitas.com>
2020-05-03 23:34:36 +0530

    files

--- Xaminals/CustomControls/Search/QuickFilter.xaml.cs
+++ Xaminals/CustomControls/Search/QuickFilter.xaml.cs
@@ -9,23 +9,21 @@ namespace Xaminals.CustomControls.Search
     [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class QuickFilter : ContentView
     {
-        private bool open;
-        double intialY = 0;
+        private bool open;     
         public QuickFilter()
         {
             InitializeComponent();
-            intialY = bottomSheet.Y;
         }
 
         async void OnTapped(object sender, EventArgs e)
         {
             if (open)
             {
-                await bottomSheet.TranslateTo(bottomSheet.X, intialY, 200, Easing.Linear);
+                MessagingCenter.Send<QuickFilter>(this, "CloseCrieteria");
             }
             else
             {
-                await bottomSheet.TranslateTo(bottomSheet.X, bottomSheet.Y - 300, 200, Easing.Linear);
+                MessagingCenter.Send<QuickFilter>(this, "ShowCrieteria");
             }
 
             open = !open;
--- Xaminals/Views/Offers/OfferList.xaml.cs
+++ Xaminals/Views/Offers/OfferList.xaml.cs
@@ -1,15 +1,37 @@
 ﻿using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
+using Xaminals.CustomControls.Search;
 
 namespace Xaminals.Views.Offers
 {
     [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class OfferList : ContentPage
     {
+        double intialY = 0;
         public OfferList()
         {
-            SetValue(NavigationPage.HasNavigationBarProperty, false);
             InitializeComponent();
+            intialY = bottomSheet.Y;
+            Subscribe();
+        }
+
+        void Subscribe()
+        {
+            MessagingCenter.Subscribe<QuickFilter>(this, "CloseCrieteria", async (obj) =>
+            {
+                var layout = bottomSheet.Bounds;
+                layout.Y = layout.Y + 200;
+                await bottomSheet.LayoutTo(layout, 250, Easing.Linear);
+
+                //await bottomSheet.TranslateTo(bottomSheet.X, intialY, 200, Easing.Linear);
+            });
+
+            MessagingCenter.Subscribe<QuickFilter>(this, "ShowCrieteria", async (obj) =>
+            {
+                var layout = bottomSheet.Bounds;
+                layout.Y = layout.Y - 200;
+                await bottomSheet.LayoutTo(layout, 250, Easing.Linear);
+            });
         }
     }
 }
